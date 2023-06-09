c0d72259e3d6976a4968e474b5f1abf16c701c91
Solitary_Alter <115763944+SolitaryEgo@users.noreply.github.com>
2023-06-09 18:19:20 +0800

    ZhihuCrawler.py

--- ZhihuCrawler.py
+++ ZhihuCrawler.py
@@ -5,8 +5,8 @@ from matplotlib.font_manager import FontProperties
 from lxml import etree
 
 headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
-    'cookie': '_zap=767d1454-0c7b-46ec-b8da-050d5a0aaa93; d_c0=AJBYkt_ELhaPTvUczNjxCN3WPO_TKQiTx-g=|1673875738; YD00517437729195%3AWM_TID=fHzdMBWVSQdFEBVVUEKBd4vJ5WGpVZim; __snaker__id=Oe8P9kngAdJ0kSku; YD00517437729195%3AWM_NI=4%2Bkpvm%2FesYRJSfCIzdORb6iQOD3VQcqIeqW57OBiS%2FxiHN1q3EcqovIeh7HHeSpy96wTRRbhQCoyWibuYetQ2HpM5HTE%2F2McOU%2BG1dT0frYs8DhcHYla%2FAl46%2FlykLSHTzY%3D; YD00517437729195%3AWM_NIKE=9ca17ae2e6ffcda170e2e6eea6d14e929caf8ef664ad928ea2d84f828a9eacc44593eaabb0b86f8f87a78ef92af0fea7c3b92af8f5a591f567edbbbdbad14291ba8daaf9708d8eaed7ec5b8d9cfdb7d469b397ab92d63ff5918e8ad225bcb788afb47bf19bad92ee7e8bbba8d8cb44a78dc09abc3eb58c9eb3e164939da19ad663a2ab9786f75bf298c0aeb844989be1d3d0698d8be5a8b47098f0aa93dc79fc9ca884cb53b0bfe1a2ef40f6f1a49aea4891989c9bdc37e2a3; q_c1=9ace7044e7864329a3417a733deaaa28|1676035079000|1676035079000; _xsrf=2b4c9541-00fd-4628-93b3-b603d4cb61b4; Hm_lvt_98beee57fd2ef70ccdd5ca52b9740c49=1683878661,1684306574,1684419604,1684485167; Hm_lpvt_98beee57fd2ef70ccdd5ca52b9740c49=1684485222; z_c0=2|1:0|10:1684485226|4:z_c0|80:MS4xMXk5VENBQUFBQUFtQUFBQVlBSlZUUmFDVTJXTWYxTk5LX3k5OU5mbkNrSkpSdjFLRXJlckN3PT0=|3ef9a66f04b9321866d2885264234f0ce543b1b618e9ecf7c4713f5309f385b4; KLBRSID=3d7feb8a094c905a519e532f6843365f|1684485243|1684485169',
+    'user-agent': '用自己的agent',
+    'cookie': '用自己的cookie',
 }
 url = 'https://www.zhihu.com/hot'
 
@@ -39,7 +39,7 @@ question_data = get_question_num(url, headers)
 
 titles = [data[1] for data in question_data]  # 提取可视化的问题标题
 
-font_path = 'C:/Windows/Fonts/msyh.ttc'  # 设置微软雅黑字体
+font_path = 'C:/Windows/Fonts/msyh.ttc'  # 设置微软雅黑字体，看自己电脑字体位置
 font_prop = FontProperties(fname=font_path)  # 设置字体属性
 
 # 创建一个问题标题的条形图
